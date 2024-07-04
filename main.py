9edd9d2bbced7acf9bbe9e98a34bfed6dd5cef8f
Juan Ludevid <jmludevid@hotmail.com>
2024-07-04 16:46:56 +1000

    update ma_cross.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
+from simulation.ma_cross import run_ma_sim
 if __name__ == '__main__':
     api = OandaApi()
 
     # data = api.get_account_summary()
     # print(data)
 
-    instrumentCollection.CreateFile(api.get_account_instruments(),"./data")
-    instrumentCollection.LoadInstruments("./data")
-    instrumentCollection.PrintInstruments()
+    # instrumentCollection.CreateFile(api.get_account_instruments(),"./data")
+    # instrumentCollection.LoadInstruments("./data")
+    # instrumentCollection.PrintInstruments()
+    run_ma_sim(curr_list=["EUR","USD","AUD"])
