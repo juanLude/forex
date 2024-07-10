8e8eae2028e7fb5d24c4cb9b5d02768be5a64af3
Juan Ludevid <jmludevid@hotmail.com>
2024-07-10 21:08:38 +1000

    create ma_excel.py

--- simulation/ma_excel.py
+++ simulation/ma_excel.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-def create_excel():
-    pass
+def create_excel(df_ma_res, df_ma_trades,granularity):
+    filename = f"{granularity}"
+
 
 if __name__ == "__main__":
-    create_excel()
+    df_ma_res = pd.read_pickle("../data/ma_res.pkl")
+    df_ma_trades = pd.read_pickle("../data/ma_trades.pkl")
+    create_excel(df_ma_res, df_ma_trades, "H1")
