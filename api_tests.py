c5cee4ae516cdba824e97543bd889e4d6d8770ec
Juan Ludevid <jmludevid@hotmail.com>
2024-08-13 07:43:02 +1000

    add open_trades() function

--- api_tests.py
+++ api_tests.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     api = OandaApi()
     instrumentCollection.LoadInstruments("./data")
     # api.place_trade("EUR_USD", 177, 1, take_profit=180, stop_loss=175)
-    trade_id = api.place_trade("EUR_USD", 2022, 1)
-    print("opened: ", trade_id)
-    time.sleep(10)
-    print(f"Closing {trade_id}", api.close_trade(trade_id))
+    # trade_id = api.place_trade("EUR_USD", 23, 1)
+    # print("opened: ", trade_id)
+    # time.sleep(10)
+    # print(f"Closing {trade_id}", api.close_trade(trade_id))   
+    [print(x) for x in api.get_open_trades()]
