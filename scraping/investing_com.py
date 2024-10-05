58506088b12f6021f7565c09fec51de8ef570dda
Juan Ludevid <jmludevid@hotmail.com>
2024-10-05 20:22:37 +1000

    get_data_object

--- scraping/investing_com.py
+++ scraping/investing_com.py
@@ -2,6 +2,34 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import cloudscraper
+
+data_keys = ['pair_name', 
+             'ti_buy', 
+             'ti_sell', 
+             'ma_buy', 
+             'ma_sell', 
+              'S1', 
+              'S2', 
+              'S3', 
+              'pivot', 
+              'R1', 
+              'R2', 
+              'R3', 
+              'percent_bullish', 
+              'percent_bearish']
+
+def get_data_object(text_list):
+    data = {}
+    for item in text_list:
+        temp_item = item.split("=")
+        if len(temp_item) == 2 and temp_item[0] in data_keys:
+            data[temp_item[0]] = temp_item[1]
+    
+    if 'pair_name' in data:
+        data['pair_name'] = data['pair_name'].replace("/", "_")
+        
+    return data
+
 def investing_com():
 
     session = cloudscraper.create_scraper()
@@ -23,6 +51,13 @@ def investing_com():
 
     text = resp.content.decode("utf-8")
 
-    
+    index_start = text.index("pair_name=")
+    index_end = text.index("*;*quote_link")
 
-    print(text)
+    data_str = text[index_start:index_end]
+
+    # split_data_str = data_str.split("*;*")
+
+    # [print(x) for x in split_data_str]
+    # keys = [x.split("=")[0] for x in split_data_str]
+    print(get_data_object(data_str.split("*;*")))
