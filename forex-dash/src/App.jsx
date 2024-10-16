337d3a604efd2a1bf65242e5e4b4af4621404249
Juan Ludevid <jmludevid@hotmail.com>
2024-10-16 15:34:43 +1100

    update navbar

---
+++ forex-dash/src/App.jsx
@@ -0,0 +1,11 @@
+import NavigationBar from "./components/NavigationBar";
+
+function App() {
+  return (
+    <div id="app-holder">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
