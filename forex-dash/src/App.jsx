6bffe3195982976096f81f75d9efce351f39961b
Juan Ludevid <jmludevid@hotmail.com>
2024-10-18 00:12:48 +1100

    add .env

--- forex-dash/src/App.jsx
+++ forex-dash/src/App.jsx
@@ -1,10 +1,22 @@
 import NavigationBar from "./components/NavigationBar";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
-    <div id="app-holder">
-      <NavigationBar />
-    </div>
+    <>
+      <BrowserRouter>
+        <div id="app-holder">
+          <NavigationBar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
