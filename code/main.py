dc23060c0cd6ff01b059d39a1b17339dd692a4a4
juanLude <jmludevid@hotmail.com>
2025-11-01 07:10:10 +1100

    update ma_cross dataframe

--- code/main.py
+++ code/main.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     #instrumentCollection.CreateFileFromApiData(api.get_account_instruments(),"./exploration")
     # instrumentCollection.LoadInstruments(".exploration/data")
     # instrumentCollection.PrintInstruments()
-    run_ma_simulation(curr_list=["EUR","USD","JPY"])
+    run_ma_simulation(curr_list=["EUR","USD", "GBP"]) 
--- code/simulation/ma_cross.py
+++ code/simulation/ma_cross.py
@@ -6,6 +6,40 @@ SELL = -1
 NONE = 0
 get_ma_col = lambda period: f'MA_{period}'
 
+class MAResult:
+    def __init__(self, df_trades, pairname, ma_l, ma_s):
+        self.pairname = pairname
+        self.df_trades = df_trades
+        self.ma_s = ma_s
+        self.ma_l = ma_l
+        self.result = self.result_ob()
+    
+    def __str__(self):
+        return str(self.result) 
+    def result_ob(self):
+        return {
+            'pair': self.pairname,
+            'num_trades': self.df_trades.shape[0],
+            'total_gain' : int(self.df_trades['GAIN'].sum()),
+            'min_gain': int(self.df_trades['GAIN'].min()),
+            'max_gain': int(self.df_trades['GAIN'].max()),
+            'ma_l': self.ma_l,
+            'ma_s': self.ma_s,
+           
+        }
+
+def process_results(results_list):
+    rl = [x.result for x in results_list]
+    df = pd.DataFrame.from_dict(rl)
+    print(df)
+def get_trades(df_analysis, instrument):
+    df_trades = df_analysis[df_analysis.TRADE != NONE].copy() 
+    df_trades['DIFF'] = df_trades.mid_c.diff().shift(-1)
+    df_trades.fillna(0, inplace=True)
+    df_trades['GAIN'] = df_trades['DIFF'] / instrument.pip_location
+    df_trades['GAIN'] = df_trades['GAIN'] * df_trades['TRADE']
+    return df_trades
+
 def is_trade(row):
     if row['DELTA'] > 0 and row['DELTA_PREV'] <= 0:
         return BUY
@@ -27,21 +61,28 @@ def assess_pair(price_data, ma_long, ma_short, instrument):
     df_analysis['DELTA'] = df_analysis[ma_short] - df_analysis[ma_long]
     df_analysis['DELTA_PREV'] = df_analysis['DELTA'].shift(1)
     df_analysis['TRADE'] = df_analysis.apply(is_trade, axis=1)
-    print(instrument.name, ma_long, ma_short)
-    print(df_analysis.tail(3))
-    return None
+    #print(instrument.name, ma_long, ma_short)
+    #print(df_analysis.tail(3))
+    df_trades = get_trades(df_analysis, instrument)
+    return MAResult(df_trades, instrument.name, ma_long, ma_short)
 def analyse_pair(instrument, granularity, ma_long,ma_short):
     ma_list = set(ma_long + ma_short)
     pair = instrument.name
-    print(f"Analysing {pair} with granularity {granularity}")
+    # print(f"Analysing {pair} with granularity {granularity}")
     price_data = load_price_data(pair, granularity, ma_list)
-    print(price_data.tail(3))
+    # print(price_data.tail(3))
 
+    results_list = []
     for ma_l in ma_long:
         for ma_s in ma_short:
-            if ma_s >= ma_l:
+            if ma_l <= ma_s:
                 continue
-            result = assess_pair(price_data, get_ma_col(ma_l), get_ma_col(ma_s), instrument)
+            ma_result = assess_pair(price_data, get_ma_col(ma_l), get_ma_col(ma_s), instrument)
+    
+            print(ma_result)
+            results_list.append(ma_result)
+    
+    process_results(results_list)
 
 
 
